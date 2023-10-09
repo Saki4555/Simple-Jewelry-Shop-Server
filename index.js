577f24975d672cf9157de3e8a106171814b3b542
saki4555 <saki999v@gmail.com>
2023-10-09 17:56:30 +0600

    cors options added

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5000;
 
